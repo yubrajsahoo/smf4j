70922f2dd4588650377d1e34e6a576969f40c9f2
yubraj <yubraj2000yubraj@gmail.com>
2026-09-19 18:37:12 +0530

    "dev-2: Add Smf4jEngineAutoConfiguration for auto-configuration of SMF4J core module"

---
+++ smf4j-engine/src/main/java/io/github/yubrajsahoo/smf4j/engine/Smf4jEngineAutoConfiguration.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2024 Yubraj Sahoo
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.yubrajsahoo.smf4j.engine;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+
+/**
+ * Auto-configuration class for the SMF4J core module.
+ * <p>
+ * This class is responsible for automatically configuring the necessary beans,
+ * aspects, and metric registries required for the Simple Metrics Facade for Java (SMF4J)
+ * when used in a Spring Boot environment.
+ * </p>
+ */
+@AutoConfiguration
+public class Smf4jEngineAutoConfiguration {
+}
